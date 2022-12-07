cfbdda992da777ead25af426a826f22a74ff90e1
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-12-07 09:54:01 +0800

    add: Create Modal input elements

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
-import { Overlay, Content, CloseButton } from './styles';
+import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton } from './styles';
 
 export function NewTransactionModal () {
   return (
@@ -17,6 +17,17 @@ export function NewTransactionModal () {
           <input type="text" placeholder="Description" required />
           <input type="number" placeholder="Price" required />
           <input type="text" placeholder="Category" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Income
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Outcome
+            </TransactionTypeButton>
+          </TransactionType>
 
           <Dialog.Close asChild>
             <button type="submit">Save</button>
